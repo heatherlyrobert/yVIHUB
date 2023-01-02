723d2b23d4a54aeef710aae8aebf2af7c92c328a
heatherlyrobert <jelloshrike@gmail.com>
2023-01-01 16:12:25 -0800

    0.1a : bring code over from yMODE and yKEYS

---
+++ Makefile
@@ -0,0 +1,46 @@
+#================================[[ beg-code ]]================================#
+
+
+
+#===(manditory)============================================#
+# basename of executable, header, and all code files
+NAME_BASE  = yVIHUB
+# additional standard and outside libraries
+LIB_STD    = 
+# all heatherly libraries used, debug versions whenever available
+LIB_MINE   = -lySTR_debug
+# directory for production code, no trailing slash
+INST_DIR   = 
+
+
+
+#===(optional)=============================================#
+# extra include directories required
+INC_MINE   = 
+# utilities generated, separate from main program
+NAME_UTIL  = 
+# libraries only for the utilities
+LIB_UTIL   = 
+
+
+
+#===(post-processing)======================================#
+# create a rule for...
+#install_prep       :
+#remove_prep        :
+
+
+
+#===(master template)======================================#
+include /usr/local/sbin/make_program
+
+
+
+#===(post-processing)======================================#
+# create a rule for...
+#install_post       :
+#remove_post        :
+
+
+
+#================================[[ end-code ]]================================#
---
+++ yVIHUB_priv.h
@@ -0,0 +1,61 @@
+/*============================----beg-of-source---============================*/
+#ifndef yVIHUB_priv
+#define yVIHUB_priv yes
+
+
+
+/*===[[ BEG_HEADER ]]=========================================================*/
+/*345678901-12345678901-12345678901-12345678901-12345678901-12345678901-12345678901-12345678901-*/
+
+/*===[[ ONE_LINERS ]]=========================================================*/
+
+#define     P_FOCUS     "VI (yvikeys provider)"
+#define     P_NICHE     "us (library integration)"
+#define     P_SUBJECT   "integration of yvikeys libraries"
+#define     P_PURPOSE   "allow extremely tight coupling of yvikeys libraries"
+
+#define     P_NAMESAKE  "telete-hagiasmos (consecration)"
+#define     P_HERITAGE  "telete is the diamona of initiation rites, daughter of dionysos"
+#define     P_IMAGERY   "young woman born from sorrow, but loves to dance and revel"
+#define     P_REASON    "initiating the contact between the closely coupled libraries"
+
+#define     P_ONELINE   P_NAMESAKE " " P_SUBJECT
+
+#define     P_BASENAME  "yVIHUB"
+#define     P_FULLNAME  "/usr/local/lib64/libyVIHUB"
+#define     P_SUFFIX    "···"
+#define     P_CONTENT   "···"
+
+#define     P_SYSTEM    "gnu/linux   (powerful, ubiquitous, technical, and hackable)"
+#define     P_LANGUAGE  "ansi-c      (wicked, limitless, universal, and everlasting)"
+#define     P_CODESIZE  "large       (appoximately 10,000 slocl)"
+#define     P_DEPENDS   "none"
+
+#define     P_AUTHOR    "heatherlyrobert"
+#define     P_CREATED   "2023-01"
+
+#define     P_VERMAJOR  "0.--, start to untangle yvikeys libraries"
+#define     P_VERMINOR  "0.1-, start with yKEYS and yMODE"
+#define     P_VERNUM    "0.1a"
+#define     P_VERTXT    "bring code over from yMODE and yKEYS"
+
+#define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
+#define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
+#define     P_REMINDER  "there are many better options, but i *own* every byte of this one"
+
+/*===[[ END_HEADER ]]=========================================================*/
+/*---(ansi-c standard)-------------------*/
+#include    <stdio.h>             /* clibc  standard input/output             */
+#include    <stdlib.h>            /* clibc  standard general purpose          */
+#include    <string.h>            /* clibc  standard string handling          */
+/*---(custom core)-----------------------*/
+#include    <yURG.h>              /* heatherly urgent processing              */
+#include    <yLOG.h>              /* heatherly program logging                */
+#include    <ySTR.h>              /* heatherly string processing              */
+
+
+
+
+
+
+#endif
